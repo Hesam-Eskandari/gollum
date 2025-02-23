d7e076ca2b60807fdee0790e7731e75c75855b77
Hesam Eskandari <eskandarihesam@gmail.com>
2025-02-23 12:30:54 -0800

    dev tax brackets interactor

---
+++ application/taxCalculator/domain/brackets/interactor/taxBracketCalculator.go
@@ -0,0 +1,54 @@
+package interactor
+
+import (
+	"github.com/Hesam-Eskandari/gollum/application/taxCalculator/domain/brackets"
+	"github.com/Hesam-Eskandari/gollum/application/taxCalculator/domain/brackets/entity"
+	"github.com/Hesam-Eskandari/gollum/application/taxCalculator/domain/constants/province"
+	"github.com/Hesam-Eskandari/gollum/application/taxCalculator/domain/errorWrap"
+	"slices"
+)
+
+type TaxBracketCalculator interface {
+	GetCalculatedTaxAsync(taxableIncome float64, year int, pr province.Province) <-chan errorWrap.ErrorWrap[float64]
+	ApplyTaxBrackets(taxableIncome float64, brackets []entity.TaxBracket) float64
+}
+
+func NewTaxBracketCalculator(dataProvider brackets.TaxBracketDataProvider) TaxBracketCalculator {
+	return &taxBracketCalculatorImpl{
+		dataProvider: dataProvider,
+	}
+}
+
+type taxBracketCalculatorImpl struct {
+	dataProvider brackets.TaxBracketDataProvider
+}
+
+func (tb *taxBracketCalculatorImpl) GetCalculatedTaxAsync(taxableIncome float64, year int, pr province.Province) <-chan errorWrap.ErrorWrap[float64] {
+	resChan := make(chan errorWrap.ErrorWrap[float64], 1)
+	go func() {
+		defer close(resChan)
+		res := errorWrap.ErrorWrap[float64]{}
+		defer func() { resChan <- res }()
+		modelEWrap := <-tb.dataProvider.GetTaxBracketsAsync(year, pr)
+		if modelEWrap.Error != nil {
+			res.Error = modelEWrap.Error
+			return
+		}
+		model := modelEWrap.Value
+		res.Value = tb.ApplyTaxBrackets(taxableIncome, model)
+	}()
+	return resChan
+}
+
+func (tb *taxBracketCalculatorImpl) ApplyTaxBrackets(taxableIncome float64, brackets []entity.TaxBracket) float64 {
+	slices.SortFunc(brackets, func(a, b entity.TaxBracket) int { return int(a.Low - b.Low) })
+	value := 0.0
+	for _, bracket := range brackets {
+		if taxableIncome < bracket.Low {
+			break
+		}
+		lowerValue := min(bracket.High, taxableIncome)
+		value += bracket.Rate * (lowerValue - bracket.Low) / 100.0
+	}
+	return value
+}
