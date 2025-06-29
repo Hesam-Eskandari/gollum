7cc430b39e36958f2a0f27386d689af766e228d0
Hesam Eskandari <eskandarihesam@gmail.com>
2025-06-28 18:45:15 -0700

    update environment.go

--- internal/environment/environment.go
+++ internal/environment/environment.go
@@ -8,29 +8,44 @@ import (
 type Environment string
 
 const (
-	Prod Environment = "Prod"
-	Dev  Environment = "Dev"
+	Prod     Environment = "prod"
+	Internal Environment = "internal"
+	Develop  Environment = "develop"
 )
 
+var defaultEnv Environment = Develop
 var environment Environment
 
 func SetEnvironment(env Environment) error {
 	if environment != "" {
 		return fmt.Errorf("environment is already set to %v", environment)
 	}
-	if env != Prod && env != Dev {
-		return fmt.Errorf("invalid environment %v. choose between %v and %v", env, Dev, Prod)
+	if env != Prod && env != Develop && env != Internal {
+		environment = defaultEnv
+		return fmt.Errorf("invalid environment \"%v\". set to default environment \"%v\"", env, defaultEnv)
 	}
 	environment = env
 	return nil
 }
 
+func fromString(env string) (Environment, error) {
+	switch env {
+	case string(Prod):
+		return Prod, nil
+	case string(Internal):
+		return Internal, nil
+	case string(Develop):
+		return Develop, nil
+	}
+	return defaultEnv, fmt.Errorf("invalid environment \"%v\", switching to \"%v\"", env, Develop)
+}
+
 func AutoSetEnvironment() error {
-	isProdEnvPtr := flag.Bool("prod", false, "is environment prod")
+	envStr := flag.String("env", string(defaultEnv), "application environment")
 	flag.Parse()
-	env := Dev
-	if *isProdEnvPtr {
-		env = Prod
+	env, err := fromString(*envStr)
+	if err != nil {
+		return err
 	}
 	return SetEnvironment(env)
 }
@@ -40,5 +55,5 @@ func GetEnvironment() Environment {
 }
 
 func IsDevelop() bool {
-	return environment == Dev
+	return environment == Develop
 }
